ec1181a086cc9175a5a799b16d4938eee14bcd37
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-01-15 23:29:05 -0300

    alterando favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@100;200;300;400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
